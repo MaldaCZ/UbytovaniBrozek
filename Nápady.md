439350b9edc2180752241c4d8b5f46e84642c5b1
MaldaCZ <vrata.maly@seznam.cz>
2025-04-22 00:15:47 +0200

    Finalizace

--- Nápady.md
+++ Nápady.md
@@ -1,3 +1,5 @@
 Features : Výlety, Pejskové
 
 Dát do odkazu Rovensko pod Troskami v sekci about us
+
+https://www.pensionkneznice.cz/ubytovani/penzion
--- main.js
+++ main.js
@@ -36,3 +36,15 @@ ScrollReveal().reveal(".header__container .btn", {
   ...scrollRevealOption,
   delay: 1000,
 });
+
+// room container
+ScrollReveal().reveal(".room__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// feature container
+ScrollReveal().reveal(".feature__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
